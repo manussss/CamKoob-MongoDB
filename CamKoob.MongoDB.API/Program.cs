f7230e3ecc3ae8d5532062da1759b1a0c79ce037
ebranquinho <branquinhomanu@gmail.com>
2025-05-08 07:47:43 -0300

    add endpoints implementation

--- CamKoob.MongoDB.API/Program.cs
+++ CamKoob.MongoDB.API/Program.cs
@@ -20,21 +20,38 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapPost("/api/v1/order", () =>
+app.MapPost("/api/v1/order", async (
+    [FromServices] IOrderRepository orderRepository,
+    Order order
+) =>
 {
-    
+    await orderRepository.CreateAsync(order);
+
+    return Results.Created();
 })
 .WithOpenApi();
 
-app.MapGet("/api/v1/order", () =>
+app.MapGet("/api/v1/order", async (
+    [FromServices] IOrderRepository orderRepository
+) =>
 {
-    
+    var orders = await orderRepository.GetAsync();
+
+    return Results.Ok(orders);
 })
 .WithOpenApi();
 
-app.MapGet("/api/v1/order/{id}", () =>
+app.MapGet("/api/v1/order/{id}", async (
+    [FromServices] IOrderRepository orderRepository,
+    Guid id
+) =>
 {
+    var order = await orderRepository.GetByIdAsync(id);
+
+    if (order is null)
+        return Results.NotFound();
     
+    return Results.Ok(order);
 })
 .WithOpenApi();
 
